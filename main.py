cf7fd7ab4c19869b275f47fb1e3f41d34ce41006
edgeboyo <mkolybko@gmail.com>
2021-09-22 17:53:39 +0200

    Added option to upload photos

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from re import split
 from config import CredentialsManager
 
+
 def handleArguments():
     import sys
+
     if len(sys.argv) != 2:
-        raise Exception('The correct usage requires a single command line argument to the facts file. Got %s' % sys.argv)
+        raise Exception(
+            "The correct usage requires a single command line argument to the facts file. Got %s"
+            % sys.argv
+        )
     return sys.argv[1]
+
+
+def handleMessage(api, facts):
+    message = facts[0]
+    attachment_url = None
+    del facts[0]
+
+    if len(facts) > 0 and facts[0][0] == "#":
+        attachment_url = facts[0][1:]
+        del facts[0]
+
+    api.PostUpdate(status=message, media=attachment_url)
+    return facts
+
 
 if __name__ == "__main__":
     api = CredentialsManager().createValidAPI()
     facts_path = handleArguments()
     with open(facts_path, "r") as f:
         facts = f.read()
-    split_facts = facts.split('\n')
-    api.PostUpdate(status=split_facts[0])
-    del split_facts[0]
+    remaining_facts = handleMessage(api, facts.split("\n"))
     with open(facts_path, "w") as f:
-        f.write("\n".join(split_facts))
+        f.write("\n".join(remaining_facts))
